6a4c7d8544207320a3af87c132767fc315f96866
Juanpav78 <juanpav.78d@gmail.com>
2022-05-26 19:18:05 -0500

    holaaa

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <h1>{{mensaje}}</h1>
+    <h2 v-text="texto"></h2>
+    <p v-html="textoHtml"> </p>
+    <input type="text" v-model="inputText">
+  </div>
 </template>
+<script>
+export default {
+ name: "App",
+ data(){
+   return {
+     mensaje: "Holaa",
+     texto: "Hola mundo!",
+     textoHtml: '<a href="https://www.google.com/webhp?hl=es&sa=X&ved=0ahUKEwj9w8jAr_73AhUYp3IEHcHiCHkQPAgI" target="_blank"> ola</a>',
+     inputText: "Escibe un texto" ,
+   }
+ },
+}
+</script>
+
+
+
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+html{
+  height: 100%;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body{
+  background-image: linear-gradient(0deg, rgba(7,180,145,0.8491879350348028) 0%, rgba(9,121,116,1) 31%, rgba(2,195,229,1) 75%, rgba(0,212,255,1) 100%);
+  height: 100%;
 }
 </style>
